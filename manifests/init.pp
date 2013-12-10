c6dee60f42223618fb6e01ca8a73bb7c98cf2857
piousbox <piousbox@nexenta-pi-desk.(none)>
2013-12-10 13:05:00 -0800

    starting to look good.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,51 @@
 
 class puppet_base {
 
+  #
+  # variables
+  #
+
+  $timeservers = [ '0.pool.ntp.org', '1.pool.ntp.org' ]
+  $snmp = { rocommunity => 'public',
+    sysDescr => 'NexentaOS',
+    sysLocation => '', 
+    sysContact => 'youremail@me.com', 
+    trapsink => 'localhost', 
+    linkUpDownNofitications => 'yes', 
+    master => 'agentx', 
+    extends => [],
+  }
+  $nameservers = [ '10.2.3.4', '10.2.3.5' ]
+  $resolv_search = 'mgt.yourcompany.net prd.yourcompany.net'
+  $domainname = $hostname
+  $loghosts = [ 'loghost1', 'loghost2' ]
+
+  # defaults -- except nfs_server_versmax where default is 4,
+  # and nfs_client_versmax where default is 4
+  $nfs = { nfsd_listen_backlog => '64', 
+    nfsd_protocol => 'ALL', 
+    nfsd_servers => '1024', 
+    lockd_listen_backlog => '64', 
+    lockd_servers => '1024', 
+    lockd_retransmit_timeout => '5', 
+    grace_pediod => '90', 
+    nfs_server_versmax => '3', 
+    nfs_client_versmax => '3',
+  }
+  $zfs_resilver_delay = '2' # default
+  $zfs_txg_synctime_ms = '5000' # default
+  $zfs_txg_timeout = '10' # default
+
+  # non-default
+  $system_extra = { swapfs_minfree => '1048576',
+    'zfs:l2arc_write_boost' => '83886080',
+  } 
+  $authorized_keys = [ 'ssh-dss AAAA...== joe', 'ssh-dss AAAnasdfg...= jane' ]
+
+  #
+  # //////////////////////////////////////////////////////////////////////////
+  #
+
   exec { 'restart_ntp':
     command => 'svcadm refresh ntp',
     path => '/',
@@ -30,7 +75,6 @@ class puppet_base {
     path => '/',
   }
 
-
   file { '/etc/nsswitch.conf':
     path => '/etc/nsswitch.conf',
     ensure => present,
@@ -40,15 +84,6 @@ class puppet_base {
     source => 'puppet:///modules/puppet_base/nsswitch.conf',
   }
 
-  # @TODO this should be documented and placed somewhere else.
-  $nameservers = [ '10.2.3.4', '10.2.3.5' ]
-
-  # @TODO document and move this elsewhere?
-  $resolv_search = 'mgt.yourcompany.net prd.yourcompany.net'
-
-  # @TODO
-  $domainname = $hostname
-
   file { '/etc/resolv.conf':
     ensure => present,
     mode => '0644',
@@ -57,7 +92,6 @@ class puppet_base {
     content => template('puppet_base/resolv.conf.erb'),
   }
 
-  # okk
   file { '/kernel/drv/scsi_vhci.conf':
     ensure => present,
     mode => '0644',
@@ -66,10 +100,6 @@ class puppet_base {
     source => 'puppet:///modules/puppet_base/scsi_vhci.conf',
   }
 
-  # @TODO move
-  $loghosts = [ 'loghost1', 'loghost2' ]
-
-  # okk
   file { '/etc/syslog.conf':
     ensure => present,
     content => template('puppet_base/syslog.conf.erb'),
@@ -79,10 +109,6 @@ class puppet_base {
     notify => Exec['restart_syslog'],
   }
 
-  # @TODO move
-  $timeservers = [ '0.pool.ntp.org', '1.pool.ntp.org' ]
-
-  # okk
   file { '/etc/inet/ntp.conf':
     ensure => present,
     content => template('puppet_base/ntp.conf.erb'),
@@ -92,14 +118,10 @@ class puppet_base {
     notify => Exec['restart_ntp'],
   }
 
-  # @TODO move
-  $snmp = { rocommunity => 'public', sysDescr => 'NexentaOS', sysLocation => '', sysContact => 'youremail@me.com', trapsink => 'localhost', linkUpDownNofitications => 'yes', master => 'agentx', extends => [] }
-
   file { '/etc/snmp':
     ensure => directory,
   }
 
-  # okk
   file { '/etc/snmp/snmpd.conf':
     ensure => file,
     # source => 'puppet:///modules/puppet_base/snmpd.conf.erb',
@@ -109,11 +131,6 @@ class puppet_base {
     mode => '0644',
     notify => Exec['restart_snmp'],
   }
-
-  # @TODO move
-  # defaults -- except nfs_server_versmax where default is 4,
-  # and nfs_client_versmax where default is 4
-  $nfs = { nfsd_listen_backlog => '64', nfsd_protocol => 'ALL', nfsd_servers => '1024', lockd_listen_backlog => '64', lockd_servers => '1024', lockd_retransmit_timeout => '5', grace_pediod => '90', nfs_server_versmax => '3', nfs_client_versmax => '3' }
 
   file { '/etc/default/nfs': 
     ensure => file,
@@ -136,13 +153,6 @@ class puppet_base {
 
   # block ses_check_flapping
 
-  # @TODO change and move
-  $version = '4.0.0'
-  $zfs_resilver_delay = '2' # default
-  $zfs_txg_synctime_ms = '5000' # default
-  $zfs_txg_timeout = '10' # default
-  $system_extra = { swapfs_minfree => '1048576', 'zfs:l2arc_write_boost' => '83886080' } # non-default
-
   file { '/etc/system':
     ensure => present,
     mode => '0755',
@@ -150,9 +160,6 @@ class puppet_base {
     group => 'root',
     content => template('puppet_base/system.erb'),
   }
-
-  # @TODO move
-  $authorized_keys = [ 'ssh-dss AAAA...== joe', 'ssh-dss AAAnasdfg...= jane' ]
 
   file { '/root/.ssh/authorized_keys':
     path => '/root/.ssh/authorized_keys',
