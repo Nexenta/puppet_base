c20ae4332eb5a29f1cc4f1f4f97c186f0f5f5e50
Victor Piousbox <piousbox@gmail.com>
2013-12-16 11:02:18 -0800

    some specs

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -11,6 +11,18 @@ describe 'default manifest' do
     true.should eql true
   end
 
+  it 'files' do
+    file_list = [ '/etc/nsswitch.conf', '/etc/resolv.conf', '/kernel/drv/scsi_vhci.conf', '/etc/syslog.conf', '/etc/inet/ntp.conf',
+                  '/etc/snmp/snmpd.conf', '/etc/default/nfs', '/etc/system', '/root/.ssh/authorized_keys' ]
+    file_list.each { |f| File.delete( f ) if File.exist?( f ) }
+    %x[ puppet agent --test ]
+    file_list.each { |f| File.exist?( f ).should eql true }
+  end
+
+  it 'services' do
+    ;
+  end
+
 end
 
 
